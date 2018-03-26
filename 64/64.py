73485a9419144e8e5ed2ae4d38ac204870a341e7
92xianshen <mr.fool@foxmail.com>
2018-03-26 20:25:31 +0800

    20180326 leetcode 64

---
+++ 64/64.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        return grid[m-1][n-1]
+    
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
+        
